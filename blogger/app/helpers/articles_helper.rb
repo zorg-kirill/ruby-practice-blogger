cbe5f024afa15ad79ab3b0c5254f075ca3ade54d
Kirill <kirill@ubuntu.(none)>
2014-02-13 23:44:48 +0200

    delete duplicates

--- blogger/app/helpers/articles_helper.rb
+++
@@ -1,5 +0,0 @@
-module ArticlesHelper
-	def article_params
-		params.require(:article).permit(:title, :body)
-	end
-end
--- blogger/config/routes.rb
+++
@@ -1,4 +0,0 @@
-Blogger::Application.routes.draw do
-  root to: 'articles#index'
-  resources :articles
-end
