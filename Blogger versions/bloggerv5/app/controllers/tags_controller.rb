4b18ecf0b78d6c017f4bb499dfc663a8c5980a2a
Kirill <kirill@ubuntu.(none)>
2014-02-13 23:28:17 +0200

    fifth version

---
+++ Blogger versions/bloggerv5/app/controllers/tags_controller.rb
@@ -0,0 +1,15 @@
+class TagsController < ApplicationController
+	def show
+		@tag=Tag.find(params[:id])
+	end
+	def index
+		@tags=Tag.all
+	end
+	def destroy
+		@tag=Tag.find(params[:id])
+		@tag.destroy
+		flash.notice="Tag #{@tag.name} was destroyed!"
+		redirect_to tags_path
+	end
+	before_filter :require_login, only: [:destroy]
+end
